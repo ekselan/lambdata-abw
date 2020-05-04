f40053e61885697b1fc52e725a7c0d81726fbd05
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-05-04 19:28:58 -0400

    1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-abw", # the name that you will install via pip
-    version="1.2",
+    version="1.3",
     author="Aaron Watkins Jr",
     author_email="aaron.watkinsjr@gmail.com",
     description="A short description",
