9078e9c22f247a4b7e2e4f50af356b624ce25c4c
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-05-05 12:30:16 -0400

    m

--- my_lambdata/state_names.py
+++ my_lambdata/state_names.py
@@ -1,7 +1,22 @@
 from pandas import DataFrame
 
-# State abbreviation -> Full Name and vice versa
-# FL -> Florida, etc
 
-df = DataFrame(['abbrev':['CA','CO','CT','DC','TX']])
-print(df.head())
+def add_state_names(my_df):
+    # State abbreviation -> Full Name and vice versa
+    # FL -> Florida, etc
+
+    new_frame = my_df.copy()
+    
+    # list or dict with abbrev/name mappings
+    names_map = {'CA':'Cali', 'CO':'Colo', 
+                'CT':'Conn', 
+                'DC':'District of Columbia'}
+
+    # Create a new column which maps the existing column
+    # using our names map
+    breakpoint()
+
+    return new_frame
+
+    df = DataFrame({'abbrev':['CA','CO','CT','DC','TX']})
+    print(df.head())
