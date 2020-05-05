192774019e55d7f193ad19af53a0d6ec85040274
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-05-05 14:02:52 -0400

    m

--- my_lambdata/polos.py
+++ my_lambdata/polos.py
@@ -24,14 +24,14 @@ class Polo():
 
 if __name__ == "__main__":
     
-# df = DataFrame(_________)
-# df.columns
-# df.head()
+    # df = DataFrame(_________)
+    # df.columns
+    # df.head()
 
-p1 = Polo(size='Small', color='blue')
-print(p1.size, p1.color)
-p1.wash
+    p1 = Polo(size='Small', color='blue')
+    print(p1.size, p1.color)
+    p1.wash
 
-p2 = Polo(size='large', color='yellow')
-print(p2.size, p2.color)
-p2.fold()
+    p2 = Polo(size='large', color='yellow')
+    print(p2.size, p2.color)
+    p2.fold()
