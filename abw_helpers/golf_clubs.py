3840a88ada8da29fed6397f43687abb5fd6ebc61
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-05-05 16:56:37 -0400

    Adds methods

--- abw_helpers/golf_clubs.py
+++ abw_helpers/golf_clubs.py
@@ -1,6 +1,12 @@
 class GolfClub():
     """
     GolfClub class
+
+    Attributes:
+        hand: str
+            example: 'left'
+        brand: str
+            example: 'TaylorMade'
     """
     def __init__(self, hand, brand):
         self.hand = hand
@@ -8,16 +14,18 @@ class GolfClub():
     
     @property
     def choose(self):
-        print('Grab the golf club')
+        print(f'Grab the {self.hand} hand {self.brand} club')
 
-    # @staticmethod
-    def swing(self):
-        print('Swing the golf club')
+    @staticmethod
+    def swing():
+        print(f'Grib it and rip it')
 
-g1 = GolfClub(hand='right', brand='Callaway')
-print(g1.hand, g1.brand)
-g1.choose
+if __name__ == "__main__":
 
-g2 = GolfClub(hand='left', brand='Ping')
-print(g2.hand, g2.brand)
-g2.swing
+    g1 = GolfClub(hand='right', brand='Callaway')
+    print(g1.hand, g1.brand)
+    g1.choose
+
+    g2 = GolfClub(hand='left', brand='Ping')
+    print(g2.hand, g2.brand)
+    g2.swing()
