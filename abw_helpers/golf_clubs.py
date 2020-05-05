46c08013adb2d9204ee578d916c6e0cd65055bf7
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-05-05 16:26:29 -0400

    m

---
+++ abw_helpers/golf_clubs.py
@@ -0,0 +1,23 @@
+class GolfClub():
+    """
+    GolfClub class
+    """
+    def __init__(self, hand, brand):
+        self.hand = hand
+        self.brand = brand
+    
+    @property
+    def choose(self):
+        print('Grab the golf club')
+
+    @staticmethod
+    def swing(self):
+        print('Swing the golf club')
+
+g1 = GolfClub(hand='right', brand='Callaway')
+print(g1.hand, g1.brand)
+g1.choose
+
+g2 = GolfClub(hand='left', brand='TaylorMade')
+print(g2.hand, g2.brand)
+g2.swing
--- my_lambdata/polos.py
+++ my_lambdata/polos.py
@@ -15,23 +15,24 @@ class Polo():
         self.size = size
         self.color = color
 
+
+
     def wash(self):
         print('Washing the polo')
         
+    @staticmethod
     def fold(self:
         print('Folding the polo')
         
 
-if __name__ == "__main__":
-    
-    # df = DataFrame(_________)
-    # df.columns
-    # df.head()
+# df = DataFrame(_________)
+# df.columns
+# df.head()
 
-    p1 = Polo(size='Small', color='blue')
-    print(p1.size, p1.color)
-    p1.wash
+p1 = Polo(size='Small', color='blue')
+print(p1.size, p1.color)
+p1.wash
 
-    p2 = Polo(size='large', color='yellow')
-    print(p2.size, p2.color)
-    p2.fold()
+p2 = Polo(size='large', color='yellow')
+print(p2.size, p2.color)
+p2.fold()
